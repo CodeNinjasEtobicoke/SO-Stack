f4955ec1a1ab0c939d0efbbf74e707480446041b
I Love Iced Coffee <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-04-23 17:47:16 -0400

    SO-complete

---
+++ Assets/Scripts/HslfwayPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HslfwayPoint : MonoBehaviour
+{
+    public Transform red;
+    public Transform blue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Calculate();
+    }
+    void Calculate()
+    {
+        Vector3 diff = red.position - blue.position;
+        diff = diff / 2f;
+
+        transform.position = (diff + blue.position);
+    }
+}
